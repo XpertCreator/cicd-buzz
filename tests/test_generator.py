74ce4dacd83b9944d9fba3b3d6de793773169c18
xshawndoe <ddwebb1@mail.mccneb.edu>
2020-11-19 15:47:53 +0000

    3 Initial commit

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -6,7 +6,7 @@ def test_sample_single_word():
     l = ('foo', 'bar', 'foobar')
     word = generator.sample(l)
     assert word in l
-assert word in 3
+    
 def test_sample_multiple_words():
     l = ('foo', 'bar', 'foobar')
     words = generator.sample(l, 2)
