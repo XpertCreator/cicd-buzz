e06d10568a735eea826cc38f819ba0388729ee3d
xshawndoe <ddwebb1@mail.mccneb.edu>
2020-11-19 15:06:13 +0000

    Add assert word in 3 in test_gen

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -6,7 +6,7 @@ def test_sample_single_word():
     l = ('foo', 'bar', 'foobar')
     word = generator.sample(l)
     assert word in l
-
+assert word in 3
 def test_sample_multiple_words():
     l = ('foo', 'bar', 'foobar')
     words = generator.sample(l, 2)
@@ -18,3 +18,4 @@ def test_sample_multiple_words():
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
+    
